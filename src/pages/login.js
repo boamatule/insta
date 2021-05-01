dffc1ecc990e313c5842a257a63d4a3365d7114c
boa <boa.matule@gmail.com>
2021-05-01 15:11:01 +0200

    Imports the logo and and image for the login page and styles it. WIP

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,7 +20,14 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>Hello Boa!</p>
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="Iphone with Instagram app" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+        </h1>
+      </div>
     </div>
   );
 }
