09d41418d7e667bf1a5523911ee8ca77c29dc1e9
boa <boa.matule@gmail.com>
2021-05-08 00:27:52 +0200

    Creates the SignUp form and router for it... WIP

--- src/seed.js
+++ src/seed.js
@@ -3,7 +3,7 @@
 export function seedDatabase(firebase) {
   const users = [
     {
-      userId: 'N19rva0iMDe4mZzIUzikCReWhnn1',
+      userId: 'MpK3wtnlJtecLfFlhpltMfxjmj92',
       username: 'Boa',
       fullName: 'Boa Matule',
       emailAddress: 'boa.matule@gmail.com',
@@ -17,7 +17,7 @@ export function seedDatabase(firebase) {
       fullName: 'Raffaello Sanzio da Urbino',
       emailAddress: 'raphael@sanzio.com',
       following: [],
-      followers: ['N19rva0iMDe4mZzIUzikCReWhnn1'],
+      followers: ['MpK3wtnlJtecLfFlhpltMfxjmj92'],
       dateCreated: Date.now()
     },
     {
@@ -26,7 +26,7 @@ export function seedDatabase(firebase) {
       fullName: 'Salvador Dalí',
       emailAddress: 'salvador@dali.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      followers: ['MpK3wtnlJtecLfFlhpltMfxjmj92'],
       dateCreated: Date.now()
     },
     {
@@ -35,7 +35,7 @@ export function seedDatabase(firebase) {
       fullName: 'George Orwell',
       emailAddress: 'george@orwell.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      followers: ['MpK3wtnlJtecLfFlhpltMfxjmj92'],
       dateCreated: Date.now()
     }
   ];
